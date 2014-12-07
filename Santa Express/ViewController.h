4dcf78a67ba3d7e03594b7d46990b475071c08d9
citlalig <citlalig@Citlalis-MacBook-Air.local>
2014-12-06 23:48:50 -0600

    Initial Commit

---
+++ Santa Express/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Santa Express
+//
+//  Created by Citlali Garcia on 12/6/14.
+//  Copyright (c) 2014 Esperanzart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Santa Express/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Santa Express
+//
+//  Created by Citlali Garcia on 12/6/14.
+//  Copyright (c) 2014 Esperanzart. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
